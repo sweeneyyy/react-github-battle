b9bc5bc2b2aa7378fdb4f8ea8142e5e025e11d1d
sweeneyyy <sweeneyarnett@gmail.com>
2018-05-03 10:43:32 -0700

    results page

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
 function PlayerPreview (props) {
   return (
@@ -114,6 +115,7 @@ class Battle extends React.Component {
     });
   }
   render() {
+    var match = this.props.match;
     var playerOneName = this.state.playerOneName;
     var playerTwoName = this.state.playerTwoName;
     var playerOneImage = this.state.playerOneImage;
@@ -152,6 +154,16 @@ class Battle extends React.Component {
                 id='playerTwo'
               />}
         </div>
+
+        {playerOneImage && playerTwoImage &&
+          <Link
+            className='button'
+            to={{
+              pathname: match.url + '/results',
+              search: `?playerOneName=` + playerOneName + `&playerTwoName=` + playerTwoName 
+            }}>
+              Battle
+          </Link>}
       </div>
     )
   }
